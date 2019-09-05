2eaa402d54a7297725ad9c59226300ebda9d82ce
Laura Goyeneche <53354917+lgoyenec@users.noreply.github.com>
2019-09-04 23:05:18 -0400

    Update app.R
    Changed titles, added notes about the variables, included downloadButton() for dataset.

--- app/app.R
+++ app/app.R
@@ -21,42 +21,61 @@ data = readRDS(paste0(cd,"/app/master_data.rds"))
 ui = tagList(
     navbarPage(
         theme = shinytheme("cerulean"),
-        title = "UNODC Violent Crimes",
+        title = strong("UNODC Violent Crimes"),
         tabPanel("Trends",
                  sidebarPanel(
                      width = 3, 
                      radioButtons("input1",
-                                  "Crime Type:",
+                                  "Select Crime Type:",
                                   choices = unique(data$Crimename)),
+                     em(p(strong("Note:"),
+                          "Apply in all tabs, including",
+                          strong("Data explorer"),
+                          "In Tab 2 corresponds to x-axis", 
+                          style = "font-size:11px")),
+                     br(),
                      radioButtons("input4",
-                                  "Crime Type 2:",
-                                  choices = unique(data$Crimename)),
+                                  "Select Crime Type:",
+                                  choices = unique(data$Crimename),
+                                  selected = "Robbery"),
+                     em(p(strong("Note:"),"Apply just for Tab 2 and corresponds to y-axis", style = "font-size:11px")),
+                     br(),
                      radioButtons("input2",
-                                  "Category:",
+                                  "Select geographical category:",
                                   choices = names(data)[1:3]),
+                     em(p(strong("Note:"),"Apply in all tabs", style = "font-size:11px")),
+                     br(),
                      radioButtons("input3",
-                                  "Measure:",
+                                  "Select indicator:",
                                   choices = c("Count","Rate")),
+                     em(p(strong("Note:"),"Apply in all tabs", style = "font-size:11px")),
+                     br(),
                      sliderInput("input5",
                                  "Year",
                                  min = min(data$Year),
                                  max = max(data$Year),
                                  value = max(data$Year),
-                                 sep = "")),
+                                 sep = ""),
+                     em(p(strong("Note:"),"Apply just for Tab 3", style = "font-size:11px"))),
                  mainPanel(
                      fluidRow(
                          column(12,
                                 h4("Gaugets")),
                          tabsetPanel(
-                             tabPanel("Trends", plotlyOutput("plot1")),
-                             tabPanel("Bubble", plotlyOutput("plot2")),
-                             tabPanel("Lollipop", plotlyOutput("plot3"))
+                             tabPanel("Tab 1", plotlyOutput("plot1")),
+                             tabPanel("Tab 2", plotlyOutput("plot2")),
+                             tabPanel("Tab 3", plotlyOutput("plot3"))
                          )
                      )
                  )
             ),
         tabPanel("Data explorer",
-                 DT::dataTableOutput("tab")),
+                 h4(strong("Crimes and rate per 1.000.000 inhabitants")),
+                 hr(),
+                 DT::dataTableOutput("tab"),
+                 hr(),
+                 h5(strong("Download data for selected variable:")),
+                 downloadButton('downloadData',"Download data")),
         tabPanel("More")
     )
 )
@@ -77,6 +96,7 @@ server = function(input, output) {
             mutate(!!sym(input$input2) := factor(!!sym(input$input2), ordered = T)) %>%
             group_by(Year,!!sym(input$input2)) %>%
             summarise(n = sum(!!sym(input$input3))) %>%
+            top_n(3, n) %>%
             plot_ly(
                 ., 
                 type = "bar",
@@ -89,16 +109,16 @@ server = function(input, output) {
                 data = temp,
                 x =~ Year, 
                 y =~ n,
-                color = I("red4"),
+                color = I("#FF8C42"),
                 type = "scatter",
+                size = I(2),
                 mode = "lines+markers",
                 inherit = F, 
-                showlegend = F
+                showlegend = T,
+                name = HTML("Annual total of crimes/total rate <br>for all regions/subregions/countries <br>")
             ) %>%
-            layout(
-                height = 600,
-                yaxis = list(title = input$input1)
-            )
+            layout(yaxis = list(title = input$input1),
+                   legend = list(font = list(size = 11)))
     })
     
     output$plot2 = renderPlotly({
@@ -116,10 +136,8 @@ server = function(input, output) {
                 color = .[[input$input2]],
                 marker = list(size =~ n3/quantile(n3,probs = 0.25), sizemode = "diameter")
             ) %>%
-            layout(
-                height = 600,
-                xaxis = list(title = input$input1),
-                yaxis = list(title = input$input4)
+            layout(xaxis = list(title = input$input1),
+                   yaxis = list(title = input$input4)
             )
     })
     
@@ -139,29 +157,38 @@ server = function(input, output) {
                 arrange(-n) %>%
                 filter(as.numeric(row.names(temp)) <= 25) %>%
                 arrange(n) %>%
-                plot_ly(color = I("gray80")) %>%
+                plot_ly(color = I("#1B4965"), size = I(8)) %>%
                 add_segments(x = 0, xend =~ n, y =~ category, yend =~ category, showlegend = F) %>%
-                add_markers(x =~ n, y =~ category, name = "", color = I("pink")) %>%
-                layout(height = 600,
-                       xaxis = list(title = input$input1),
+                add_markers(x =~ n, y =~ category, name = "", color = I("#FFC857"), size = 120) %>%
+                layout(xaxis = list(title = input$input1),
                        yaxis = list(title = ""))
         } else {
             temp %>%
-                plot_ly(color = I("gray80")) %>%
+                plot_ly(color = I("#1B4965"), size = I(8)) %>%
                 add_segments(x = 0, xend =~ n, y =~ category, yend =~ category, showlegend = F) %>%
-                add_markers(x =~ n, y =~ category, name = "", color = I("pink")) %>%
-                layout(height = 600, 
-                       xaxis = list(title = input$input1),
+                add_markers(x =~ n, y =~ category, name = "", color = I("#FFC857"), size = 250) %>%
+                layout(xaxis = list(title = input$input1),
                        yaxis = list(title = ""))
         }
     })
     
+    dataFilter = reactive({
+        data = 
+            data %>%
+            filter(Crimename == input$input1)
+            
+    })
+    
     output$tab = DT::renderDataTable({
-        DT::datatable(data, 
-                      options = list(pageLength = 20), 
+        DT::datatable(dataFilter(),
+                      options = list(pageLength = 10), 
                       rownames = F)
     })
     
+    output$downloadData = downloadHandler(
+        filename = function() {paste("data-", Sys.Date(), ".csv", sep = "")},
+        content = function(file) {write.csv(dataFilter(),file)}
+    )
 }
 
 # Run app
